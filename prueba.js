7fb34b916fd3de72bdf86defa8a0869a453f3d7f
Eduardo913 <cesarvilla903@gmail.com>
2021-11-26 00:46:06 -0600

    arreglo de automatas

--- prueba.js
+++ prueba.js
@@ -1,29 +1,17 @@
-var timed = 0;
-var scrollGo = false;
-var delay=100;
-var space=100;
-function scroll_start (){
-  var i=0;
-  msg="Éste es el mensaje mostrado en la barra de estado";
-  for (i=0; i<space; i++){
-    msg=" "+msg;
-    scrol1Go=true;
-    timerid=window.setTimeout( "scrollmsg (0)", delay);
-  }
-
+function Animal (nombre) {
+  
 }
- 
-function scrollmsg(pos){
-  var out = "";
-  scrol1Go=false;
-  if (pos < msg.length){
-    self.status = msg.substring(pos, msg.length);
+class Perro extends Animal {
+  hablar(nombre) {
+    super.hablar();
+    this.nombre = nombre;
+    console.log(this.nombre + ' ladra.');
   }
-  else{
-    pos=-1;
-    ++pos;
-    scrol1GotIue;
-    timerid=window.setTimeout( "scrollmsg( "+pos+")", delay);
-  }
+}
+
+var p = new Perro('Mitzie');
+p.hablar();
+
+switch(nombre){
 
 }
